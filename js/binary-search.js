f940a844998aacb29fd1285e877d2b6c5633434b
Aniruddha Mahamuni <aniruddha27@gmail.com>
2022-12-05 07:38:49 +0530

    Implemented binary Search

--- js/binary-search.js
+++ js/binary-search.js
@@ -1,4 +1,4 @@
-function binarySearch(arr, ele) {
+function binarySearch1(arr, ele) {
     let start = 0;
     let end = arr.length - 1;
     let mid = Math.floor((start + end) / 2);
@@ -13,7 +13,43 @@ function binarySearch(arr, ele) {
     return (arr[mid] === ele) ? mid : -1;
 }
 
+
+
+function binarySearch(arr, elem) {
+    let start = 0;
+    let end = arr.length - 1;
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2)
+        if (arr[mid] < elem) {
+            start = mid + 1;
+        } else if (arr[mid] > elem) {
+            end = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+
+//binarySearch Recursive
+function binarySearchRecurive(arr, elem, start, end) {
+    if (start > end) return -1;
+
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] < elem) {
+        return binarySearchRecurive(arr, elem, mid + 1, end)
+    } else if (arr[mid] > elem) {
+        return binarySearchRecurive(arr, elem, start, mid - 1)
+    } else {
+        return mid;
+    }
+}
+
+
 const nums = [1, 2, 3, 5, 7, 9, 10, 14, 20];
 const search = 9;
 
+console.log(binarySearch1(nums, search));
 console.log(binarySearch(nums, search));
+console.log(binarySearchRecurive(nums, search, 0, nums.length - 1));
